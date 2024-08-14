0250754e4459a69b5d4480e107a21a4811ce7275
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2024-08-14 17:09:55 -0300

    initial Spring Boot Project

---
+++ Java/src/main/java/br/com/locaweb/locamail/api/LocaMailApplication.java
@@ -0,0 +1,13 @@
+package br.com.locaweb.locamail.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaMailApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/br/com/locaweb/locamail/api/LocaMailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.locaweb.locamail.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
